5ebb467b337622bbcd2374a0f1a3483f290cebd6
niiyazobug <niiyaz5555@gmail.com>
2022-03-13 15:15:25 +0600

    file change

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 import pyttsx3
 import PyPDF2
-book = open('oopp.pdf', 'rb')
+book = open('pdf/book.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
-friend = pyttsx3.init()
+bro = pyttsx3.init()
 for number in range(7, pages):
     page = pdfReader.getPage(number)
     text = page.extractText()
