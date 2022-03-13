88c79c4c053fde24d18bbc479a72c3e240c7c044
niiyazobug <niiyaz5555@gmail.com>
2022-03-13 15:02:51 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import pyttsx3
+import PyPDF2
+book = open('oopp.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+friend = pyttsx3.init()
+for num in range(7, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    friend.say(text)
+    friend.runAndWait()
